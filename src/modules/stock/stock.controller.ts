479275bf9858dff46efec88a46cbbac01e92bb32
lucasfcs <lucas.fcs@outlook.com>
2024-04-22 13:18:49 -0300

    adding documentation in output

--- src/modules/stock/stock.controller.ts
+++ src/modules/stock/stock.controller.ts
@@ -1,17 +1,26 @@
 import { Controller, Get, Query } from '@nestjs/common';
-import { ApiTags } from '@nestjs/swagger';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
 import { StockService } from './stock.service';
 
 @ApiTags('Stock')
 @Controller('stock')
 export class StockController {
   constructor(private readonly stockService: StockService) {}
+
+  @ApiOperation({
+    summary: 'get all stock by name',
+    description: 'route that searches the stock by name',
+  })
   @Get('find-by-name')
   async findByName(@Query('name') name: string): Promise<any> {
     const result = await this.stockService.findByName(name);
     return result;
   }
 
+  @ApiOperation({
+    summary: 'route that searches all products in stock',
+    description: 'route that returns a complete list of all products in stock',
+  })
   @Get()
   async findAll(): Promise<any> {
     const result = await this.stockService.findAll();
